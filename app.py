dd60e896f2c77b4edb39cd18141bd67c6d19067f
sargisheiser <sargis.heiser@gmail.com>
2025-10-02 13:09:32 +0200

    Added Author and Title funcitons

--- app.py
+++ app.py
@@ -1,16 +1,76 @@
 import os
-from flask import Flask
-from data_models import db, Author
+from datetime import datetime
 
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from data_models import db, Author, Book
 
+app = Flask(
+    __name__,
+    static_url_path="/static",
+    static_folder=os.path.join(os.path.dirname(__file__), "static"),
+    template_folder=os.path.join(os.path.dirname(__file__), "templates")
+)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+
+@app.route("/")
+def home():
+    sort_by = request.args.get("sort", "title")
+    if sort_by == "author":
+        books = Book.query.join(Author).order_by(Author.name).all()
+    else:
+        books = Book.query.order_by(Book.title).all()
+    return render_template("home.html", books=books)
+
+
+@app.route("/add_author", methods=["GET", "POST"])
+def add_author():
+    message = None
+    if request.method == "POST":
+        name = request.form["name"]
+        birth_date_str = request.form.get("birth_date")
+        death_date_str = request.form.get("date_of_death")
+
+        birth_date = datetime.strptime(birth_date_str, "%Y-%m-%d").date() if birth_date_str else None
+        date_of_death = datetime.strptime(death_date_str, "%Y-%m-%d").date() if death_date_str else None
+
+        new_author = Author(name=name, birth_date=birth_date, date_of_death=date_of_death)
+        db.session.add(new_author)
+        db.session.commit()
+        message = f"Author '{name}' added successfully!"
+
+    return render_template("add_author.html", message=message)
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    authors = Author.query.order_by(Author.name).all()
+
+    if request.method == "POST":
+        isbn = request.form.get("isbn")
+        title = request.form.get("title")
+        publication_year = request.form.get("publication_year")
+        author_id = request.form.get("author_id")
+
+        new_book = Book(
+            isbn=isbn,
+            title=title,
+            publication_year=publication_year,
+            author_id=author_id,
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
+        message = f" Book '{title}' added successfully!"
+        return render_template("add_book.html", authors=authors, message=message)
+
+    return render_template("add_book.html", authors=authors)
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        print("Tables created successfully!")
+    app.run(debug=True)
